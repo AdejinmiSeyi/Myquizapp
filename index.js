6681eef7635ac5ff8358b8baf0ee63dc8ed187b8
AdejinmiSeyi <seyifeyisara@gmail.com>
2023-05-27 13:28:01 +0100

    initial commit for quiz app

---
+++ index.js
@@ -0,0 +1,66 @@
+const Questions = [
+    {
+        questionId: 1,
+        question: "Which of these is NOT a South Africa national park?",
+        options: [{
+            text: "Addo Elephant Park", answer: "false",
+            text: "Kruger National Park", answer: "false",
+            text: "Table Mountain National Park", answer: "false",
+            text: "Goreme National Park", answer: "true"
+        }
+
+        ]
+    }
+
+    {
+        questionId: 2,
+        question: "Which of these is NOT a South Africa national park?",
+        options: [{
+            text: "Addo Elephant Park", answer: "false",
+            text: "Kruger National Park", answer: "false",
+            text: "Table Mountain National Park", answer: "false",
+            text: "Goreme National Park", answer: "true"
+        }
+
+        ]
+    }
+
+    {
+        questionId: 3,
+        question: "Which of these is NOT a South Africa national park?",
+        options: [{
+            text: "Addo Elephant Park", answer: "false",
+            text: "Kruger National Park", answer: "false",
+            text: "Table Mountain National Park", answer: "false",
+            text: "Goreme National Park", answer: "true"
+        }
+
+        ]
+    }
+
+    {
+        questionId: 4,
+        question: "Which of these is NOT a South Africa national park?",
+        options: [{
+            text: "Addo Elephant Park", answer: "false",
+            text: "Kruger National Park", answer: "false",
+            text: "Table Mountain National Park", answer: "false",
+            text: "Goreme National Park", answer: "true"
+        }
+
+        ]
+    }
+
+    {
+        questionId: 5,
+        question: "Which of these is NOT a South Africa national park?",
+        options: [{
+            text: "Addo Elephant Park", answer: "false",
+            text: "Kruger National Park", answer: "false",
+            text: "Table Mountain National Park", answer: "false",
+            text: "Goreme National Park", answer: "true"
+        }
+
+        ]
+    }
+]
